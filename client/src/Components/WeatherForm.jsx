23e70679acd81a3e9030c4beff37da534d4e034c
Wei He <breehope@me.com>
2021-09-23 11:44:13 -0700

    Refacted components to use redux for stage mgmt.

--- client/src/Components/WeatherForm.jsx
+++ client/src/Components/WeatherForm.jsx
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 import {Form, Button, Row, Col, ButtonGroup, ToggleButton} from "react-bootstrap";
 import axios from 'axios';
 
+// imports redux to manage the stage(/props)
+import {connect} from "react-redux";
+import {saveZipCode, saveWeatherData, saveTemperature, updateHistory} from "../actions";
+
 class WeatherForm extends Component {
     // default state values
     state = {
@@ -20,6 +24,7 @@ class WeatherForm extends Component {
                 zipCode: localStorage.getItem("zipCode"),
                 tempMetric: localStorage.getItem("tempMetric")
             }).then(d => {
+                this.props.saveWeatherData(d.data);
                 localStorage.setItem("CurrentWeatherData", JSON.stringify(d.data));
             });
         }
@@ -37,20 +42,22 @@ class WeatherForm extends Component {
         }).then(response => {
             let weatherData = response.data;
 
+            this.saveToStore(weatherData);
             this.saveToLocalStorage(weatherData);
         });
     }
 
     // Save data from form to local storage
     saveToLocalStorage = (weatherData) => {
-        localStorage.setItem("zipCode", this.state.zipCodeInput);
-        localStorage.setItem("tempMetric", this.state.tempMetric);
+        // localStorage.setItem("zipCode", this.state.zipCodeInput);
+        // localStorage.setItem("tempMetric", this.state.tempMetric);
         localStorage.setItem("CurrentWeatherData", JSON.stringify(weatherData));
+        localStorage.setItem("WeatherHistory", JSON.stringify(this.props.history));
     }
 
-    onChange = (e) => {
-        this.setState({[e.target.name]: e.target.value});
-    }
+    // onChange = (e) => {
+    //     this.setState({[e.target.name]: e.target.value});
+    // }
 
     saveToMongo = (event) => {
         axios.post("/api/weatherMongo", {
@@ -63,10 +70,26 @@ class WeatherForm extends Component {
         });
     }
 
+    // Saves data to the Redux store
+    saveToStore = (weatherData) => {
+        this.props.saveTemperature(this.state.tempMetric);
+        this.props.saveZipCode(this.state.zipCodeInput);
+        this.props.saveWeatherData(weatherData);
+
+        this.props.updateHistory({
+            timestamp: (new Date()).toLocaleString(),
+            city: weatherData.name,
+            zipcode: this.state.zipCodeInput,
+            temperature: weatherData.main.temp,
+            description: weatherData.weather[0].description
+        });
+    }
+
     render() {
         return (
-            // <Form className="weather-form" onSubmit={this.saveFormData}> // save to localStorage
-            <Form className="weather-form" onSubmit={this.saveToMongo}> 
+            // save to localStorage
+            <Form className="weather-form" onSubmit={this.saveFormData}>  
+            {/* <Form className="weather-form" onSubmit={this.saveToMongo}>  */}
 
                 <Row type="flex" justify="center" align="center" className="zipCode">
                     <Col>
@@ -121,4 +144,28 @@ class WeatherForm extends Component {
     }
 }
 
-export default WeatherForm;
+// Mapping state from the store to props;
+// meaning...if we update these props, it'll update the redux store
+const mapStateToProps = (state) => {
+    return {
+        zipCode: state.zipCode,
+        weather: state.weather,
+        tempMetric: state.tempMetric,
+        history: state.history
+    }
+};
+
+// These are the actions we can dispatch and just mapping it to props
+const mapDispatchToProps = () => {
+    return {
+        saveZipCode,
+        saveWeatherData,
+        saveTemperature,
+        updateHistory
+    }
+};
+
+// export default WeatherForm;
+
+// This connects our mapping the state & dispatch to props to use in WeatherForm
+export default connect(mapStateToProps, mapDispatchToProps())(WeatherForm);
