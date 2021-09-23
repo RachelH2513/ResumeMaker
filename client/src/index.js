6b94c95b166ef7550ef7e28ef4e572044e89d327
Wei He <breehope@me.com>
2021-09-23 11:42:53 -0700

    Added redux actions and reducers

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,23 @@ import App from './App';
 import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Adding redux and reducers 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
+// Creates our store to use our reducers and the chrome extension to debug the redux store
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Header />
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
