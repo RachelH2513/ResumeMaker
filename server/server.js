b02d9dd739a8bb50ca007aa517d70895110acf09
Wei He <breehope@me.com>
2021-09-23 23:46:57 -0700

    Use inbuild body parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,11 +9,24 @@ const connectDB = require('../config/db');
 connectDB();
 
 // use body parser to get data from POST requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded())
 
 // Use API routes from the api folder
 const apis = require("./api");
 app.use("/api", apis);
+
+const path = require('path');
+
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
