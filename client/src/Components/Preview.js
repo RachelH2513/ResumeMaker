f0bb7a1d20d431d2bd24dcaa3addcb68cb33718b
Wei He <breehope@me.com>
2021-09-27 01:09:20 -0700

    Adjusted style

--- client/src/Components/Preview.js
+++ client/src/Components/Preview.js
@@ -32,7 +32,7 @@ class Preview extends Component {
                         {data.educations.map((element, k) => 
                             <div key={k}>
                                 <div style={{display: 'flex'}}>
-                                    <h5>
+                                    <h5 style={{fontWeight:'bold'}}>
                                         {element.school}
                                     </h5>
                                     <h5 style={{float:'right', marginLeft:'auto'}}>
@@ -56,11 +56,11 @@ class Preview extends Component {
                 {/* Experience Section */}
                 {data.experiences.length !== 0 
                     ? <div>
-                        <h3>
+                        <h3 style={{marginBottom:'10px', marginTop:'8px'}}>
                             Experience
                         </h3>
                         {data.experiences.map((element, k) => 
-                            <div style={{padding:'5px'}} key={k}>
+                            <div key={k}>
                                 <div style={{display: 'flex'}}>
                                     <h5 style={{'fontWeight':'bold'}}>
                                         {element.company}&emsp;|&emsp;{element.position}
@@ -69,7 +69,7 @@ class Preview extends Component {
                                         {moment(element.from).format("MMM YYYY")}&nbsp;-&nbsp;{moment(element.to).format("MMM YYYY")}, {element.location}   
                                     </h5>
                                 </div>
-                                <h5>
+                                <h5 style={{textAlign:'justify'}}>
                                     {element.desc}
                                 </h5>
                             </div>
